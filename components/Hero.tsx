5b64675b53f6752466fdb5663d919a831dc5429d
dzr01145 <dzr01145@nifty.com>
2025-11-27 23:33:11 +0900

    Maximize profile image hover trigger area to the left

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -209,7 +209,7 @@ const Hero: React.FC = () => {
         {/* Visual/Image Area - Absolute positioned for overlap */}
         <div className="hidden lg:block absolute right-0 top-1/2 -translate-y-1/2 w-[45%] h-[80%] z-10 pointer-events-auto group flex items-center justify-center">
           {/* Invisible Hover Trigger Area - Extends significantly to the left */}
-          <div className="absolute inset-y-0 -left-60 w-60 z-20"></div>
+          <div className="absolute inset-y-0 -left-[500px] w-[500px] z-20"></div>
 
           <div className="relative w-full h-full">
             <div className="relative w-[90%] h-[90%]">
